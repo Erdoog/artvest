a88dc861874c89e257546b58515a267858218ac6
aaalliii <116755476+aaalliii@users.noreply.github.com>
2024-07-02 21:34:36 +0500

    Add files via upload

---
+++ lib/widgets/educational_page.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+class Video {
+  final String link;
+  final String description;
+
+  Video({required this.link, required this.description});
+
+  factory Video.fromJson(Map<String, dynamic> json) {
+    return Video(
+      link: json['link'],
+      description: json['description'],
+    );
+  }
+}
+
+class VideoPage extends StatefulWidget {
+  @override
+  _VideoPageState createState() => _VideoPageState();
+}
+
+class _VideoPageState extends State<VideoPage> {
+  late Future<List<Video>> futureVideos;
+
+  @override
+  void initState() {
+    super.initState();
+    futureVideos = fetchVideos();
+  }
+
+  Future<List<Video>> fetchVideos() async {
+    final String response = await rootBundle.loadString('assets/videos.json');
+    final data = await json.decode(response);
+    return (data as List).map((video) => Video.fromJson(video)).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Education'),
+      ),
+      body: FutureBuilder<List<Video>>(
+        future: futureVideos,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+            return Center(child: Text('No videos available'));
+          } else {
+            return ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final video = snapshot.data![index];
+                return VideoCard(video: video);
+              },
+            );
+          }
+        },
+      ),
+    );
+  }
+}
+
+class VideoCard extends StatefulWidget {
+  final Video video;
+
+  VideoCard({required this.video});
+
+  @override
+  _VideoCardState createState() => _VideoCardState();
+}
+
+class _VideoCardState extends State<VideoCard> {
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      initialVideoId: YoutubePlayer.convertUrlToId(widget.video.link)!,
+      flags: YoutubePlayerFlags(
+        autoPlay: false,
+        mute: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 2,
+            child: YoutubePlayer(
+              controller: _controller,
+              showVideoProgressIndicator: true,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.video.description),
+          ),
+        ],
+      ),
+    );
+  }
+}
